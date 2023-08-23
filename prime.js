3fce3ab6dc8561e75a6ae2a1efb0e2daee48913b
sameeryadav123 <sameervasantyadav@gmail.com>
2023-08-23 20:28:38 +0530

    code added

--- prime.js
+++ prime.js
@@ -8,3 +8,19 @@ let chekingIndex = (arr, indexN) => {
 
 const animals = ["Lion", "Tiger", "Bear", "Elephant", "Giraffe"];
 chekingIndex(animals, "Tiger");
+
+
+
+function customSubstring(str,st,ed){
+    if(ed === undefined){
+        ed=str.length
+    }
+    let bag="";
+    for(let i=st; i<ed;i++){
+        bag+=str[i]
+    }
+    return bag;
+}
+let str="Mozilla";
+console.log(customSubstring(str,1,3))
+console.log(customSubstring(str,2))
